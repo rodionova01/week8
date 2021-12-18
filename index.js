3258c2f79742c4bf2bf4439636a765465acc2e37
Irina <irarodeonova@mail.ru>
2021-12-18 19:13:16 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const exspress = require('express')
+const bodyParser = require('body-parser')
+const app = exspress()
+app.use(bodyParser.urlencoded())
+
+const port = process.env.PORT || 8080
+
+app.get('/', (req, res) =>
+{
+    res.render('index.ejs')
+})
+
+app.post('/', (req, res) =>
+{
+    const username = req.body.username
+    res.render('hello.ejs', {username: username})
+}
+)
+app.listen(port, ()=> console.log(`listen port:${port}`))
